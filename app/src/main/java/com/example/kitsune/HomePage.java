69fbeb3e8d9eb989c6e35b8a62db968762bead5f
Caitlin Lim <caitlim777@gmail.com>
2023-07-18 12:53:11 -0400

    Added routes to workout and social pages

--- app/src/main/java/com/example/kitsune/HomePage.java
+++ app/src/main/java/com/example/kitsune/HomePage.java
@@ -10,6 +10,9 @@ import android.widget.Button;
 public class HomePage extends AppCompatActivity {
     private Button onlineStoreButton;
     private Button monitorHealthButton;
+    private Button workoutsButton;
+    private Button socialNetworkButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +33,22 @@ public class HomePage extends AppCompatActivity {
                 openHealthInfo();
             }
         });
+
+        workoutsButton = (Button) findViewById(R.id.workoutsButton);
+        workoutsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openWorkoutInfo();
+            }
+        });
+
+        socialNetworkButton = (Button) findViewById(R.id.socialNetworkButton);
+        socialNetworkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSocialNetworkInfo();
+            }
+        });
     }
     public void openShoppingActivity() {
         Intent intent = new Intent(HomePage.this, ShoppingActivity.class);
@@ -37,6 +56,14 @@ public class HomePage extends AppCompatActivity {
     }
     public void openHealthInfo() {
         Intent intent = new Intent(HomePage.this, HealthInfo.class);
+        startActivity(intent);
+    }
+    public void openWorkoutInfo() {
+        Intent intent = new Intent(HomePage.this, WorkoutPage.class);
+        startActivity(intent);
+    }
+    public void openSocialNetworkInfo() {
+        Intent intent = new Intent(HomePage.this, FeedActivity.class);
         startActivity(intent);
     }
 }
